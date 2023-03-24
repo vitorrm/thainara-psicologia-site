025da5747cea0307b03a736ba8497a59a5688ef3
Vitor <vitor.rm@gmail.com>
2023-03-24 15:41:04 -0300

    test

---
+++ app/pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="home">
+      <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+         Test VRM
+      </div>
+    </section>
+</template>
+  
+<script lang="ts">
+</script>
+  
